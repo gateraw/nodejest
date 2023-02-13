f27978b50e4129f7fc6ac586c353b0e4d80e3bf4
gateraw <106636420+gateraw@users.noreply.github.com>
2023-02-13 17:49:23 +0000

    update docker-image-resource blob

--- setup-fonts.sh
+++ setup-fonts.sh
@@ -11,4 +11,8 @@ rm -Rf $TMPDIR
 mkdir -p $TMPDIR
 
 # Install python fontforge bindings for use by powerline font patcher:
-if [ $(dpkg-query -W -f='${Status}' python3-fontforge 2>/dev/null | grep -c "ok installed") -eq 0 ]; t
+if [ $(dpkg-query -W -f='${Status}' python3-fontforge 2>/dev/null | grep -c "ok installed") -eq 0 ]; then
+	sudo apt install python3-fontforge
+fi
+
+FONTPATCHER_DIR=$HOME/src
